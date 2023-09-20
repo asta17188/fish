650a512b3d2c7a98f486f33a5dd2cdf325a61d42
asta17188 <112020427+asta17188@users.noreply.github.com>
2023-09-20 02:55:31 +0000

    fish

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -3,11 +3,17 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(5, 163, 255);
-  }
+  background(220);
+  fill(255, 191, 0);
+  ellipse(200, 200, 100, 50)
+  beginShape();
+    vertex(120,180);
+    vertex(120, 223);
+    vertex(180, 205);
+  endShape(CLOSE);
+  fill(255, 255, 255);
+  ellipse(230, 190, 15, 15);
+  fill(0, 0, 0);
+  ellipse(230, 190, 5, 5);
 
-  ellipse(mouseX, mouseY, 80, 80);
 }
